92c8c810fe3033b623acc19a021418e93e74a2b6
arxycth <dimasadiluhur@gmail.com>
2025-12-23 06:49:03 +0700

    fix bug

---
+++ public/build/assets/globe-Cwq9W1Sq.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-B60J3u6x.js";const e=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],h=o("ChevronRight",e);const c=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20",key:"13o1zl"}],["path",{d:"M2 12h20",key:"9i4pu4"}]],a=o("Globe",c);export{h as C,a as G};
